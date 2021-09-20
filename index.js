ccff0279699ab50fd2ec7ac102094bcf7d87ed6a
ownfrezzy <ownfrezzy@gmail.com>
2021-09-20 16:57:19 +0300

    +set_up

--- index.js
+++ index.js
@@ -4,26 +4,6 @@ require("dotenv").config();
 const port = process.env.PORT || 3000;
 const routes = require("./routes/index");
 const bodyParser = require("body-parser");
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: "TYP API",
-      description: "TYP-web API",
-      contact: {
-        name: "Kiryl Sachuk",
-        email: "ownfrezzy@gmail.com",
-      },
-      servers: ["http://localhost:3300"],
-      version: "1.0.1",
-    },
-  },
-  apis: ["./routes/*.js"],
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(bodyParser.json()); 
 
