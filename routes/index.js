58fb528cd01a78abb9f6972b381af8f6df9db7f0
ownfrezzy <ownfrezzy@gmail.com>
2021-09-22 15:53:20 +0300

    +modules crud

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const usersRoutes = require('./usersRoutes.js')
 const studentRoutes = require('./studentRoutes.js')
+const modulesRoutes = require('./modulesRoutes')
 
 router.use('/users', usersRoutes )
 router.use('/students', studentRoutes)
+router.use('/modules', modulesRoutes)
 
 module.exports = router;
