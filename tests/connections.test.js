2cc77ec8f9ea3a0c81c138cfa690c5d7b131c67d
ownfrezzy <ownfrezzy@gmail.com>
2021-10-01 12:32:17 +0300

    +conections tests

--- tests/connections.test.js
+++ tests/connections.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const app = require("../index");
-const { Students } = require("../models/_models");
+const { Students, StudentModules } = require("../models/_models");
 const { SocialNetworks } = require("../models/_models");
 function deleteLast(database) {
   database
@@ -8,9 +8,9 @@ function deleteLast(database) {
       order: [["createdAt", "DESC"]],
       where: {},
     })
-    .then((user) =>
+    .then((obj) =>
       database.destroy({
-        where: { id: user.id },
+        where: { id: obj.id },
         limit: 1,
       })
     );
@@ -55,6 +55,38 @@ describe("SOCIAL NETWORKS creating", () => {
       const response = await request(app).post("/api/students").send({
         fullName: "Supertest34",
         user_id: 11293824776,
+      });
+      expect(response.statusCode).toBe(500);
+    });
+  });
+});
+
+describe("STUDENT_MODULES creating", () => {
+  describe("When student and module already exists", () => {
+    test("Should response with status 200 and obj containing StudentModule id", async () => {
+      const response = await request(app).post("/api/studentmodules").send({
+        module_id: 1,
+        student_id: 1,
+      });
+      deleteLast(StudentModules);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.id).toBeDefined();
+    });
+  });
+  describe("When such user not exists", () => {
+    test("Should response with status 500", async () => {
+      const response = await request(app).post("/api/studentmodules").send({
+        module_id: 1,
+        student_id: 213812465126378,
+      });
+      expect(response.statusCode).toBe(500);
+    });
+  });
+  describe("When such module not exists", () => {
+    test("Should response with status 500", async () => {
+      const response = await request(app).post("/api/studentmodules").send({
+        module_id: 11231264712839210,
+        student_id: 1,
       });
       expect(response.statusCode).toBe(500);
     });
